863c3fa0128fd038dfbcb3eadad9c774d84885ad
Prakhar Tiwari <prakhartew@gmail.com>
2020-10-27 19:24:05 +0530

    minor fix in routing - ScrollToTop

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ import PrivateLimited from "./landing-pages/private-limited/PrivateLimited";
 import OnePersonCompany from "./landing-pages/one-person-company/OnePersonCompany";
 import LimitedLiabilityCompany from "./landing-pages/limited-liability-company/LimitedLiabilityCompany";
 import SectionEight from "./landing-pages/section-eight/SectionEight";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <div class="main-container">
       <Router>
         <Navbar />
-        <Switch>
-          <Route path="/private-limited" component={PrivateLimited} />
-          <Route path="/limited-liability" component={LimitedLiabilityCompany} />
-          <Route path="/one-person-company" component={OnePersonCompany} />
-          <Route path="/section-eight-company" component={SectionEight} />
-          <Route path="/" component={HomePage} />
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route path="/private-limited" component={PrivateLimited} />
+            <Route
+              path="/limited-liability"
+              component={LimitedLiabilityCompany}
+            />
+            <Route path="/one-person-company" component={OnePersonCompany} />
+            <Route path="/section-eight-company" component={SectionEight} />
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </ScrollToTop>
         <FeedbackAndServices />
       </Router>
     </div>
