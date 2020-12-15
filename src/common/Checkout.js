0f4ff27a4353dfd596f5ab99f894575083f02508
Prakhar Tiwari <prakhartew@gmail.com>
2020-12-15 23:23:52 +0530

    minor change

--- src/common/Checkout.js
+++ src/common/Checkout.js
@@ -175,21 +175,6 @@ function Checkout() {
           </form>
         </div>
       </div>
-
-      <footer className="my-5 pt-5 text-muted text-center text-small">
-        <p className="mb-1">&copy; 2017-2019 Company Name</p>
-        <ul className="list-inline">
-          <li className="list-inline-item">
-            <a href="#">Privacy</a>
-          </li>
-          <li className="list-inline-item">
-            <a href="#">Terms</a>
-          </li>
-          <li className="list-inline-item">
-            <a href="#">Support</a>
-          </li>
-        </ul>
-      </footer>
     </div>
   );
 }
