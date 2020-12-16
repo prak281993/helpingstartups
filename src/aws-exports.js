12fe8e73f9ac9e5b66e7b26da899e74ad9238e50
Prakhar Tiwari <prakhartew@gmail.com>
2020-12-16 19:58:09 +0530

    created new amplify env

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,12 +3,12 @@
 
 const awsmobile = {
     "aws_project_region": "ap-south-1",
-    "aws_cognito_identity_pool_id": "ap-south-1:0c6f77c7-37b0-4050-b564-5bec7aa43e9e",
+    "aws_cognito_identity_pool_id": "ap-south-1:edd3e1e2-7c3d-48e2-9a21-c63807f0480b",
     "aws_cognito_region": "ap-south-1",
-    "aws_user_pools_id": "ap-south-1_j18ORb03V",
-    "aws_user_pools_web_client_id": "70dovbm5sosp9nok6slgeqi3dl",
+    "aws_user_pools_id": "ap-south-1_raR5EOWN2",
+    "aws_user_pools_web_client_id": "189qpo50esva6vba8h3goeju33",
     "oauth": {
-        "domain": "helpingstartups8ad54258-8ad54258-prod.auth.ap-south-1.amazoncognito.com",
+        "domain": "helpingstartups-prod.auth.ap-south-1.amazoncognito.com",
         "scope": [
             "phone",
             "email",
@@ -16,18 +16,11 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "https://helpingstartups.netlify.app/",
-        "redirectSignOut": "https://helpingstartups.netlify.app/",
+        "redirectSignIn": "https://helpingstartups.netlify.com/",
+        "redirectSignOut": "https://helpingstartups.netlify.com/",
         "responseType": "code"
     },
-    "federationTarget": "COGNITO_USER_POOLS",
-    "aws_cloud_logic_custom": [
-        {
-            "name": "helpingstartups",
-            "endpoint": "https://u9hjeiungh.execute-api.ap-south-1.amazonaws.com/prod",
-            "region": "ap-south-1"
-        }
-    ]
+    "federationTarget": "COGNITO_USER_POOLS"
 };
 
 
