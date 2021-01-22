2c1586bd3a0c9af59d2edda2aeb4a5d6c1d4e3b6
Prakhar Tiwari <prakhartew@gmail.com>
2021-01-23 01:17:48 +0530

    added buildspec for yaml

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/",
-        "redirectSignOut": "http://localhost:3000/",
+        "redirectSignIn": "https://helpingstartup.in/",
+        "redirectSignOut": "https://helpingstartup.in/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
--- src/landing-pages/private-limited/PrivateLimited.js
+++ src/landing-pages/private-limited/PrivateLimited.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { StickyContainer } from "react-sticky";
 import FeedbackAndServices from "../../common/FeedbackAndServices";
 import LandingPagesHeader from "../../common/LandingPagesHeader";
